cf1a150dd6f8193ad355bd00ec734465afc30e66
Marta <martasolerebri@gmail.com>
2026-02-17 20:40:22 +0100

    update

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from langchain_core.runnables import RunnablePassthrough
 
 st.set_page_config(page_title="Goodreads AI Librarian", layout="wide")
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style.css")
+
 st.title("Goodreads AI Librarian")
 
 with st.sidebar:
